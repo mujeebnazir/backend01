1037ba2ae8764837131c57989be0dd5d26fd9ca9
mujeebnazir <95225496+mujeebnazir@users.noreply.github.com>
2023-12-09 13:26:05 +0530

    databse connected

---
+++ src/index.js
@@ -0,0 +1,30 @@
+//require('dotenv').config({path: './env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+connectDB();
+
+//always use try catch for better code to connect to database
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR: ", error);
+    throw err;
+  }
+})();
+*/
