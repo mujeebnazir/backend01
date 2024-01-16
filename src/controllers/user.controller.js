d3778f6c8ce64b775085ee04abc3d947c89f6be7
mujeebnazir <95225496+mujeebnazir@users.noreply.github.com>
2024-01-16 17:42:44 +0530

    bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -169,7 +169,9 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: undefined },
+      $unset: {
+        refreshToken: 1, //this removes field from document
+      },
     },
     {
       new: true,
@@ -263,12 +265,12 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 const updateAccountDetails = asyncHandler(async (req, res) => {
   const { fullName, email } = req.body;
-  if (!fullName || !email) {
+  if (!(fullName || email)) {
     throw new ApiError(400, "Fullname or email is required");
   }
 
